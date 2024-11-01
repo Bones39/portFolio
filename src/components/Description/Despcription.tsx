7ab6588de38956da8395b20382435dd5227a497e
Andraina <asz_andraina@yahoo.fr>
2024-11-01 09:56:49 +0100

    add new description paragraph with lorem

---
+++ src/components/Description/Despcription.tsx
@@ -0,0 +1,12 @@
+const Description = () => {
+	return <p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel iusto vero dolorum,
+		minus saepe sit quasi magni cum eius, doloremque incidunt tenetur unde in delectus repudiandae
+		sapiente? Exercitationem animi fugit deserunt reprehenderit, voluptates rem velit vel numquam dolor,
+		ullam quas minus. Incidunt saepe facere totam magnam ipsam quibusdam labore ab debitis ullam, pariatur quaerat,
+		quos nihil dolor omnis laborum sit aspernatur minus, deserunt placeat. Assumenda doloremque nobis laboriosam natus
+		voluptatem minus! Deserunt obcaecati similique esse dolor, cupiditate eveniet ipsa accusamus placeat voluptatibus
+		hic tenetur repellat sapiente blanditiis praesentium magni vitae vel corporis officia, debitis non expedita adipisci ipsum quos.
+	</p>
+}
+export default Description;
--- src/components/DescriptionContainer/DescriptionContainer.tsx
+++ src/components/DescriptionContainer/DescriptionContainer.tsx
@@ -1,33 +1,13 @@
 import { useEffect, useState } from "react";
 import DescriptionHeader from "../DecriptionHeader/DescriptionHeader";
+import Description from "../Description/Despcription";
 
 const DescriptionContainer = ()	=> {
-	// let iteration = 0;
-	const [index, setIndex] = useState(0);
-	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const jobs = ["front end web designer", "free lancer"];
-	const [displayJob, setDisplayJob] = useState(jobs[1]);
-	// const [randomInitialDisplay, setRandomInitialDisplay] = useState("test");
-	const [innerDisplayJob, setInnerDisplayJob] = useState("randomInitialDisplay");
-	const [changeIndex, setChangeIndex] = useState(() => false); // this state will only be modified by the child component when it finishes rendering
-
-	useEffect(() => {
-		setIndex((index + 1) % jobs.length);
-		console.log("from parent " + jobs[index]);
-		var random = jobs[index]
-			.split("")
-			.map((letter, index) => {
-				return letters[Math.floor(Math.random()*52)];
-			})
-			.join("");
-		setDisplayJob(jobs[index]);
-		setInnerDisplayJob(random);
-		console.log("change Index " + changeIndex);
-	}, [changeIndex]);
 	
 	return(
 		<div className="descriptionContainer">
-			<DescriptionHeader displayJob={displayJob} changeIndex={changeIndex} setChangeIndex={setChangeIndex} setInnerDisplayJob={setInnerDisplayJob} innerDisplayJob={innerDisplayJob}></DescriptionHeader>
+			<DescriptionHeader></DescriptionHeader>
+			<Description></Description>
 		</div>
 	)
 }
