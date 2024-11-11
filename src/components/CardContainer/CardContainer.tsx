38cd3f832592c411cc0fca1a06e94f44008a9fed
ANDRIAMISEZA Andraina <aandriamiseza@lateca.fr>
2024-11-11 17:01:51 +0100

    open a modal popup when a project card is clicked

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import styles from "./CardContainer.module.css";
+import Modal from "../Modal/Modal";
 
 interface props {
 	title: string;
@@ -6,17 +8,29 @@ interface props {
 }
 
 const CardContainer = ({ title, description }: props) => {
+
+	const [modal, setModal] = useState(false);
+
+	const toggleModal = () => {
+		setModal(!modal);
+	  };
+
 	return (
-	<article className={styles.card}>
-		<header>
-			{<h2>{title}</h2>}
-		</header>
-		<div className={styles.description}>
-			{description}
-		</div>
-		<div>
-			Here the list of used technologies
-		</div>
-	</article>)
+		<>
+			<article className={styles.card} onClick={() => toggleModal()}>
+				<header>
+					{<h2>{title}</h2>}
+				</header>
+				<div className={styles.description}>
+					{description}
+				</div>
+				<div>
+					Here the list of used technologies
+				</div>
+			</article>
+
+			{modal && (<Modal modal={modal} toggleModal={toggleModal}></Modal>)}
+		</>
+	)
 }
 export default CardContainer;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import styles from "./Modal.module.css";
+
+/* create a modal component: https://www.youtube.com/watch?v=9DwGahSqcEc */
+
+interface Props {
+    modal: boolean;
+    toggleModal: () => void;
+}
+
+export default function Modal({modal, toggleModal}: Props) {
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  return (
+    <>
+      <div className={styles.modal}>
+          <div onClick={toggleModal} className={styles.overlay}></div>
+          <div className={styles["modal-content"]}>
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores? Illum pariatur possimus
+              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+              placeat tempora vitae enim incidunt porro fuga ea.
+            </p>
+            <button className={styles["close-modal"]} onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+    </>
+  );
+}
