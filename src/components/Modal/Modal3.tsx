3eca3071c7a211e4c115fb42d44a964061288e02
Andraina <asz_andraina@yahoo.fr>
2024-11-26 08:49:26 +0100

    implement simulation

--- src/components/Modal/Modal3.tsx
+++ src/components/Modal/Modal3.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import "./FlexModal.css"
 
 type CellState = boolean;
@@ -22,6 +22,20 @@ const FlexModal = () => {
 	const [position, setPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
 	const [dragging, setDragging] = useState<boolean>(false);
   	const [origin, setOrigin] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+	const [running, setRunning] = useState<boolean>(false);
+
+
+	// define const variables
+	const runningRef = useRef(running);
+	const containerRef = useRef<HTMLDivElement>(null);
+	runningRef.current = running;
+
+	// Neighbor positions relative to a cell
+	const neighborPositions = [
+		[-1, -1], [-1, 0], [-1, 1], // Top row
+		[0, -1],          [0, 1],   // Middle row
+		[1, -1], [1, 0], [1, 1]     // Bottom row
+	];
 
 	// ------------------------ event handlers ---------------------
 	const handleMouseDown = (event: React.MouseEvent) => {
@@ -55,10 +69,71 @@ const FlexModal = () => {
 		border: `solid 1px blue`
 	}
 
+	// ---------------------- Simulation function -----------------
+	const runSimulation = useCallback(() => {
+		if (!runningRef.current) {
+			return;
+		}
+
+		setGrid((currentGrid) => {
+			return currentGrid.map((row, rowIndex) =>
+			row.map((cell, colIndex) => {
+				let neighbors = 0;
+				neighborPositions.forEach(([x, y]) => {
+				const newRow = rowIndex + x;
+				const newCol = colIndex + y;
+				if (
+					newRow >= 0 &&
+					newRow < numRows &&
+					newCol >= 0 &&
+					newCol < numCols
+				) {
+					neighbors += currentGrid[newRow][newCol] ? 1 : 0;
+				}
+				});
+
+				// Apply Conway's rules
+				if (cell) {
+				return neighbors === 2 || neighbors === 3;
+				} else {
+				return neighbors === 3;
+				}
+			})
+			);
+		});
+
+		setTimeout(runSimulation, 100);
+  	}, []);
+
 	return (
 		<div className="flexWindowsContainer">
 			<div id="controlBox" className="box">
-				<div className="controlButtonPane"></div>
+				<div className="controlButtonPane">
+					<button onClick= {() => {
+						setRunning(!running);
+						if (!running) {
+							runningRef.current = true;
+							runSimulation();
+						}
+					}}>
+						{running ? 'Stop' : 'Start'}
+					</button>
+					<button
+						onClick={() => {
+						setGrid(generateEmptyGrid());
+						}}>
+						Clear
+					</button>
+					<button
+						onClick={() => {
+							const randomGrid = Array.from({ length: numRows }, () =>
+								Array.from({ length: numCols }, () => Math.random() > 0.7)
+							);
+							setGrid(randomGrid);
+						}}>
+						Random
+					</button>
+				</div>
 				<div className="controlButtonPane"></div>
 				<div className="controlButtonPane"></div>
 				<div className="controlButtonPane"></div>
