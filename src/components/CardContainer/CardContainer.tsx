1d0ac0572a7fc4d62c30f5b1711cea0758d2f385
Andraina <asz_andraina@yahoo.fr>
2024-12-01 12:44:18 +0100

    clear the code

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 import styles from "./CardContainer.module.css";
-import Modal from "../Modal/Modal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFontAwesome, faReact, faBootstrap } from '@fortawesome/free-brands-svg-icons'
 import IconTags from "../IconTags/IconTags";
-import Modal2 from "../Modal/Modal2";
-import GameOfLife from '../GameOfLife/GameOfLife';
-import GameOfLife2 from "../GameOfLife/GameOfLife2";
-import ZoomableContent from "../GameOfLife/ZoomableContent";
-import FlexModal from "../Modal/Modal3";
+import ModalWindow from "../Modal/ModalWindow";
+import GameOfLife from "../Modal/GameOfLife";
 // import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 interface props {
@@ -22,10 +18,6 @@ const CardContainer = ({ title, description, usedTechnologies }: props) => {
 
 	const [isOpen, setIsOpen] = useState(false);
 
-/* 	const toggleModal = () => {
-		setModal(!modal);
-	  }; */
-
 	return (
 		<>
 			<article className={styles.card} onClick={() => setIsOpen(true)}>
@@ -37,11 +29,11 @@ const CardContainer = ({ title, description, usedTechnologies }: props) => {
 				</div>
 				<IconTags iconNames={usedTechnologies}></IconTags>
 			</article>
-			<Modal2 open={isOpen} onClose={() => setIsOpen(false)} title={title}>
+			<ModalWindow open={isOpen} onClose={() => setIsOpen(false)} title={title}>
 				{/* <GameOfLife2/> */}
 				{/* <ZoomableContent/> */}
-				<FlexModal></FlexModal>
-			</Modal2>
+				<GameOfLife></GameOfLife>
+			</ModalWindow>
 		</>
 	)
 }
